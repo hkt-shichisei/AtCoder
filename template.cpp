a103644411f80184748dc14cab44b415a55eec21
hkt-shichisei <sota20070612@gmail.com>
2025-05-02 20:28:06 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define all(a) (a).begin(), (a).end()
+using ll = long long;
+const ll INF32 = 2e9;
+const ll INF64 = 4e18;
+
+void printYN(bool ok){
+    if(ok)cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return;
+}
+
+int main() {
+    return 0;
+}
